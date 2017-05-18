c49f3168dbaeaafa0496e8366350ba95b817528b
elixander <lilithswu@gmail.com>
2017-05-18 02:17:59 -0700

    Adding some graphics.

--- src/components/CatHouse.jsx
+++ src/components/CatHouse.jsx
@@ -2,9 +2,9 @@ import classNames from 'classnames';
 import React from 'react';
 
 const BUBBLE_POSITIONS = [
-	'cat-house__speech-bubble--position1', 
-	'cat-house__speech-bubble--position2', 
-	'cat-house__speech-bubble--position3',
+	'cat-house__speech-bubble--left', 
+	'cat-house__speech-bubble--center', 
+	'cat-house__speech-bubble--right',
 ];
 
 const BUBBLE_WORDS = [
@@ -17,6 +17,9 @@ const BUBBLE_WORDS = [
 
 const BUBBLE_TIMEOUT_LENGTH = 2000;
 
+const NUM_FRAMES = 3;
+const MAX_FRAME = NUM_FRAMES * 2 - 1;
+
 class CatHouse extends React.Component {
 	constructor(props){
 		super(props);
@@ -24,6 +27,8 @@ class CatHouse extends React.Component {
 			speech: null,
 			bubblePosition: null,
 			timeout: null,
+			interval: null,
+			frame: null,
 		};
 	}
 
@@ -31,6 +36,12 @@ class CatHouse extends React.Component {
 		const bubbleClasses = classNames('cat-house__speech-bubble', {
 			'cat-house__speech-bubble--visible': !!this.state.speech, 
 			[this.state.bubblePosition]: true,
+		});
+
+		const whichImage = this.state.frame > NUM_FRAMES ? 
+				(NUM_FRAMES - (this.state.frame % NUM_FRAMES)) : this.state.frame;
+		const catClasses = classNames('cat-house__cat', {
+			[`cat-house__cat--speaking-${whichImage}`]: !!this.state.interval,
 		});
 
 		// This is a shitty way to do this, but it's not the most important thing, so. 
@@ -41,7 +52,10 @@ class CatHouse extends React.Component {
 				<div className="cat-house__speech-container">
 					<div className={bubbleClasses}>{this.state.speech}</div>
 				</div>
-				<div className="cat-house__cat" onClick={() => this.beep()}/> 
+				<div className={catClasses} onClick={() => {
+					this.beep(); 
+					this.animate();
+				}}/> 
 				{showFacetimeLink && (
 					<div className="cat-house__call">
 						<a href="facetime:14802771985">Facetime Cat</a>
@@ -51,10 +65,30 @@ class CatHouse extends React.Component {
 		);
 	}
 
+	animate(){
+		// TODO: Change this to use requestAnimationFrame.
+		if (this.state.interval) this.stopAnimating();
+
+		const step = (timestamp) => {
+			const nextFrame = (this.state.frame || 0) + 1;
+
+			if (nextFrame > MAX_FRAME) {
+				this.stopAnimating();
+			} else {
+				this.setState({frame: nextFrame});
+			}
+		};
+
+		this.setState({interval: window.setInterval(step, 50)});
+	}
+
+	stopAnimating(){
+		window.clearInterval(this.state.interval);
+		this.setState({interval: null, frame: null});
+	}
+
 	beep(){
-		if (this.state.timeout !== null){
-			this.unBeep();
-		}
+		if (this.state.timeout) this.unBeep();
 
 		const bubblePositionIndex = Math.floor(Math.random() * BUBBLE_POSITIONS.length);
 		const bubbleWordIndex = Math.floor(Math.random() * BUBBLE_WORDS.length);
